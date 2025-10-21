538e9a070731b5cbba7475965cf36c5a31f09915
Tihago-bit <pukara2007@gmail.com>
2025-10-21 14:38:13 -0300

    Correcion de errores

--- My-pagina-main/My-pagina-main/my-app/src/App.jsx
+++ My-pagina-main/My-pagina-main/my-app/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
       marginTop: "100px",
       fontFamily: "Arial, sans-serif"
     }}>
-      <h1>Bienvenido a BookFlix</h1>
-      <p>Tu plataforma para explorar libros como si fueran películas.</p>
-      <p style={{ color: "gray" }}>Si ves este mensaje, React funciona correctamente </p>
+      <h1>Bienvenido al proyecto</h1>
+      <p>Es como un netflix pero de libros</p>
+      <p style={{ color: "red" }}>Si ves esto funciona bien</p>
     </div>
   );
 }
