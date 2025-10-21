b8afc5c38f41489a02ce01c4a390263f148af983
Tihago-bit <pukara2007@gmail.com>
2025-10-21 15:11:31 -0300

    Solucion de errores 2

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{
+      textAlign: "center",
+      marginTop: "100px",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <h1>Bienvenido al proyecto</h1>
+      <p>Es como un netflix pero de libros</p>
+      <p style={{ color: "red" }}>Si ves esto funciona bien</p>
+    </div>
+  );
+}
+
+export default App;
